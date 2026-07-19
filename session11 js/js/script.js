9d46556c03fdc29ba2724f2add49f18386576e60
Rwan Yasser <totorwan8@gmail.com>
2026-07-20 01:03:24 +0300

    javascript  degree test

---
+++ session11 js/js/script.js
@@ -0,0 +1,64 @@
+// console.log("javascript")
+// var firstName="rwan"
+// var lastName="yasser"
+// console.log(firstName +" "+ lastName)
+// console.log(`my full name is
+//      ${firstName} ${lastName}`)
+     
+// var age=10
+// var num=50
+// var sum = age + num
+// console.log(sum)
+
+//  var isGraduated=false
+//  console.log(typeof isGraduated)
+
+//   var p= null
+//   console.log(p)
+//   console.log(firstName/lastName)
+
+// // increment
+// console.log("increment")
+// var l=10
+// console.log(l) //10
+// console.log(l++) //10
+// console.log(l) //11
+// console.log(++l) //12
+// console.log("----------------------------")
+
+// //change value
+// var num=5
+// // num=num+2
+// num+=2
+// console.log(num)
+
+// //comparison operations
+// console.log('comparison operators')
+// var num1=40
+// var num2=50
+// var num3=40
+// console.log(num1 > num2)
+// console.log(num1 <= num3)
+// var test=20
+// var test2="20"
+// console.log(test == test2) //value
+// console.log(test === test2)//value , type
+// console.log("------------")
+// console.log(test != test2)
+// console.log(test !== test2)
+
+// console.log("----------------------------")
+
+// //typeof
+
+// prompt("hi")
+// alert("welcome")
+var degree= prompt("enter degree")
+// if(prompt(degree)){
+if(degree >= 0 && degree <= 100){
+if(degree >= 90) alert('A')
+else if(degree >= 80 && degree < 90) alert("B")
+else if(degree >= 70 && degree < 80) alert("C")
+else if(degree < 70)alert("F")
+// }
+}else alert("not valid degree")
